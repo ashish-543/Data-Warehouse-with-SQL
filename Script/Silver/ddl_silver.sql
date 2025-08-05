a1af22f32859ee3e0ff6e924f97d07665f094492
ashish-543 <ashpnd543@gmail.com>
2025-08-05 19:01:37 +0545

    Creating tables in silver schema

--- Script/Silver/ddl_silver.sql
+++ Script/Silver/ddl_silver.sql
@@ -22,6 +22,10 @@ CREATE TABLE silver.crm_cust_info (
     cst_create_date     DATE,
     dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
+/*
+DEFAULT GETDATE(): This sets the default value of the column to the current date and time when a new row is inserted without explicitly providing a value
+for this column.
+*/
 GO
 
 
@@ -35,9 +39,9 @@ CREATE TABLE silver.crm_prd_info(
 	prd_nm			  VARCHAR(50),
 	prd_cost		  VARCHAR(50),
 	prd_line		  VARCHAR(50),
-	prd_start_dt	DATETIME,
-	prd_end_dt		DATETIME,
-	dwh_create_date    DATETIME2 DEFAULT GETDATE()
+	prd_start_dt		  DATETIME,
+	prd_end_dt		  DATETIME,
+	dwh_create_date   	  DATETIME2 DEFAULT GETDATE()
 	);
 GO
 
@@ -47,16 +51,16 @@ IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT,
-	  dwh_create_date    DATETIME2 DEFAULT GETDATE()
+    sls_ord_num  	NVARCHAR(50),
+    sls_prd_key  	NVARCHAR(50),
+    sls_cust_id  	INT,
+    sls_order_dt 	INT,
+    sls_ship_dt  	INT,
+    sls_due_dt   	INT,
+    sls_sales    	INT,
+    sls_quantity 	INT,
+    sls_price    	INT,
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -66,9 +70,9 @@ IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_loc_a101 (
-    cid    NVARCHAR(50),
-    cntry  NVARCHAR(50),
-	  dwh_create_date    DATETIME2 DEFAULT GETDATE()
+    cid    		NVARCHAR(50),
+    cntry  		NVARCHAR(50),
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -78,10 +82,10 @@ IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_cust_az12 (
-    cid    NVARCHAR(50),
-    bdate  DATE,
-    gen    NVARCHAR(50),
-	  dwh_create_date    DATETIME2 DEFAULT GETDATE()
+    cid    		NVARCHAR(50),
+    bdate  		DATE,
+    gen    		NVARCHAR(50),
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -91,10 +95,10 @@ IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
-    cat          NVARCHAR(50),
-    subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50),
-	  dwh_create_date    DATETIME2 DEFAULT GETDATE()
+    id           	NVARCHAR(50),
+    cat          	NVARCHAR(50),
+    subcat       	NVARCHAR(50),
+    maintenance  	NVARCHAR(50),
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
